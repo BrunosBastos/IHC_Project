f17388cdbed9b344603b9dba770dadafef025c7b
Pedro <pedromiglou@hotmail.com>
2020-05-19 19:56:55 +0100

    knockout myjobs

---
+++ js/myJobs.js
@@ -0,0 +1,36 @@
+function MyViewModel() {
+    var self = this;
+    self.originOffers = ko.observableArray(JSON.parse(localStorage.getItem("offers")));
+    self.offers = ko.observableArray();
+    for (i=0; i< self.originOffers().length; i++) {
+        if (self.originOffers()[i].email.localeCompare(localStorage.getItem("current_user"))) {
+            console.log(self.originOffers()[i])
+            self.offers().push(self.originOffers()[i])
+        }
+    }
+    console.log(self.offers())
+
+    self.goToJobDetails = function(offer) {
+        localStorage.setItem("current_offer", JSON.stringify(offer))
+        window.location.href = "./job_details_recruiter.html"
+    }
+}
+ko.applyBindings(new MyViewModel());
+
+(function ($) {
+    $(document).ready(function(){
+        var scroll2candidates;
+
+        //if ()
+
+        $(".js-candidates").click(function(e){
+            // Prevent a page reload when a link is pressed
+            e.preventDefault();
+
+            // Scroll
+            $('html,body').animate({
+                scrollTop: 300
+            }, 'slow');
+        });
+    })
+})(jQuery);
