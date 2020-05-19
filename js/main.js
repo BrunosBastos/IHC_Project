e943b8237b7d52254865d9213e313d3d113b6da7
Pedro <pedromiglou@hotmail.com>
2020-05-19 23:39:00 +0100

    database update

--- js/main.js
+++ js/main.js
@@ -341,12 +341,17 @@ $(document).ready(function() {
       {"educationLevel": "High School Diploma or Equivalent","name":"Leonardo Lenny", "dataNasc":"25/05/1976", "city":"Nápoles", "address":"Viale della libertà", "email":"lenny@gmail.com", "password":"passpass", "phoneNumber":"923456789", "description":"- Experient in cooking", "type":"Others"},
       {"educationLevel": "Bachelor’s Degree", "name":"Leandro Silva", "dataNasc":"26/06/2000", "city":"Aveiro", "address":"Rua D. Joana", "email":"leandro@gmail.com", "password":"passpass", "phoneNumber":"929876543", "description":"degree in computer science", "type":"Software & Web"}];
   localStorage.setItem("seekers", JSON.stringify(seekers));
+
   let recruiters =[{"name" : "Bastos e Irmãos", "email":"bastoseirmaos@gmail.com", "password":"passpass", "address":"Rua D. Ana", "city":"Aveiro", "phone" : "932222222"},
       {"name" : "Amaral Limitada", "email":"amaral@gmail.com", "password":"passpass", "address":"Avenida da Liberdade", "city":"Aveiro", "phone" : "932255552"},
       {"name":"Heinz Group", "email":"heinz@gmail.com", "password":"passpass", "address":"Viale della libertà", "city":"Nápoles", "phone":"961234589"}]
   localStorage.setItem("recruiters", JSON.stringify(recruiters));
-  let offers = [{"id":1,"email":"bastoseirmaos@gmail.com", "date":"11/11/2011", "role":"Engenheiro Informático", "city":"Aveiro", "address":"Rua D. Maria", "category":"Software & Web", "jobNature":"Full-time", "salary":"1200", "vacancy":"3", "description":""}];
+
+  let offers = [{"id":1,"email":"bastoseirmaos@gmail.com", "date":"10/05/2020", "role":"Engenheiro Informático", "city":"Aveiro", "address":"Rua D. Maria", "category":"Software & Web", "jobNature":"Full-time", "salary":"1200", "vacancy":"3", "description":""},
+      {"id":2,"email":"amaral@gmail.com", "date":"11/05/2020", "role":"Designer", "city":"Aveiro", "address":"Bairro de Santiago", "category":"Software & Web", "jobNature":"Full-time", "salary":"1100", "vacancy":"2", "description":"In need of a designer for a website"},
+      {"id":2,"email":"heinz@gmail.com", "date":"12/05/2020", "role":"Cousine Chef", "city":"Nápoles", "address":"Viale della libertà", "category":"Others", "jobNature":"Full-time", "salary":"1050", "vacancy":"1", "description":"In need of a chef for a hotel kitchen"}];
   localStorage.setItem("offers", JSON.stringify(offers))
+
   let applications = [{"email": "chiquito_420@sapo.pt", "id":1, "status":"Accepted", "date":"19/05/2020"}];
   localStorage.setItem("applications", JSON.stringify(applications))
   let current_user = "";
