20ad3cbde2efe4487bf3dd3a06701605057d91af
Pedro <pedromiglou@hotmail.com>
2020-05-20 17:57:26 +0100

    applications update

--- js/index.js
+++ js/index.js
@@ -1,4 +1,3 @@
-
 function MyViewModel() {
     // database send to local storage - seekers, employers, offers, applications
     if ((localStorage.getItem("seekers")) === null) {
@@ -21,7 +20,7 @@ function MyViewModel() {
           {"id":6,"email":"bastoseirmaos@gmail.com", "date":"17/05/2020", "role":"Empregada de Limpeza", "city":"Aveiro", "address":"Rua D. Maria", "category":"Others", "jobNature":"Part-time", "salary":"650", "vacancy":"1", "description":"Procura-se uma empregada de limpeza"}];
       localStorage.setItem("offers", JSON.stringify(offers))
     
-      let applications = [{"email": "chiquito_420@sapo.pt", "id":1, "status":"Accepted", "date":"19/05/2020"}];
+      let applications = [{"email": "leandro@gmail.com", "id":1, "status":"A", "date":"19/05/2020"}];
       localStorage.setItem("applications", JSON.stringify(applications))
       let current_user = "";
       localStorage.setItem("current_user",current_user);
--- js/job_details_seeker.js
+++ js/job_details_seeker.js
@@ -17,12 +17,12 @@ function MyViewModel() {
 ko.applyBindings(new MyViewModel());
 
 (function ($) {
-    $(document).ready(function(){
+    $(document).ready(function () {
         var scroll2candidates;
 
         //if ()
 
-        $(".js-candidates").click(function(e){
+        $(".js-candidates").click(function (e) {
             // Prevent a page reload when a link is pressed
             e.preventDefault();
 
@@ -31,18 +31,92 @@ ko.applyBindings(new MyViewModel());
                 scrollTop: 300
             }, 'slow');
         });
-    })
 
-    $("#applyButton").click(function() {
-        if ($("#applyButton").hasClass("info")) {
-            $("#applyButton").removeClass("info");
-            $("#applyButton").addClass("danger");
-            $("#applyButton").text("Unapply")
-        } else {
-            $("#applyButton").removeClass("danger");
-            $("#applyButton").addClass("info");
-            $("#applyButton").text("Apply")
+        var applications = JSON.parse(localStorage.getItem("applications"))
+        var current_user = localStorage.getItem("current_user")
+        var current_offer = JSON.parse(localStorage.getItem("current_offer"))
+        var applied = false
+        var accepted = false
+        var rejected = false
+
+        for (i = 0; i < applications.length; i++) {
+            if (applications[i].email.localeCompare(current_user) == 0) {
+                if (applications[i].id == current_offer.id) {
+                    applied = true;
+                    if (applications[i].status == "A") {
+                        accepted = true
+                    } else {
+                        if (applications[i].status == "R") {
+                            rejected = true
+                        }
+                    }
+                }
+            }
         }
 
+        if (!rejected) {
+            if (accepted) {
+                $("#applyButton").text("Accepted")
+                $("#applyButton").removeClass("info");
+                $("#applyButton").addClass("btn-success");
+                $("#applyButton").addClass("disabled")
+            } else {
+                if (applied) {
+                    $("#applyButton").removeClass("info");
+                    $("#applyButton").addClass("danger");
+                    $("#applyButton").text("Unapply")
+                } else {
+                    $("#applyButton").removeClass("danger");
+                    $("#applyButton").addClass("info");
+                    $("#applyButton").text("Apply")
+                }
+            }
+        } else {
+            $("#applyButton").text("Rejected")
+            $("#applyButton").removeClass("info");
+            $("#applyButton").addClass("danger");
+            $("#applyButton").addClass("disabled")
+        }
+    })
+
+    $("#applyButton").click(function () {
+        if (!($("#applyButton").hasClass("disabled"))) {
+            var applications = JSON.parse(localStorage.getItem("applications"))
+            var current_user = localStorage.getItem("current_user")
+            var current_offer = JSON.parse(localStorage.getItem("current_offer"))
+            var applied = false
+            var toDel
+
+            for (i = 0; i < applications.length; i++) {
+                if (applications[i].email.localeCompare(current_user) == 0) {
+                    if (applications[i].id == current_offer.id) {
+                        toDel = i
+                        applied = true;
+                    }
+                }
+            }
+
+            if (applied) {
+                $("#applyButton").removeClass("danger");
+                $("#applyButton").addClass("info");
+                $("#applyButton").text("Apply")
+                var newArray = []
+                for (i=0; i<applications.length; i++) {
+                    if (i!=toDel) {
+                        newArray.push(applications[i])
+                    }
+                }
+                applications = newArray;
+            } else {
+                $("#applyButton").removeClass("info");
+                $("#applyButton").addClass("danger");
+                $("#applyButton").text("Unapply")
+                var d = new Date();
+                applications.push({ "email": current_user, "id": current_offer.id, "status": "P", "date": d.getDate() + "/" + (d.getMonth() + 1) + "/" + d.getFullYear() })
+            }
+
+            localStorage.setItem("applications", JSON.stringify(applications))
+            console.log(applications)
+        }
     })
 })(jQuery);
